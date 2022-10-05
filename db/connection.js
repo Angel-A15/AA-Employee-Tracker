c3cc712ef6baaa7953638948c6a4edacb13545ad
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-10-05 08:25:12 -0500

    added to connection js

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,18 @@
 //Connects MySQL to database
 const mysql = require('mysql2');
 
-//Connect to database
+//Connect to database with credentials
 const db = mysql.createConnection (
-    
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
     {
         host: 'localhost',
         //Your MySQL Username
         user: '',
         //Your Password
         password: '',
-        database: 'department'
+        database: 'employee_tracker'
     },
     console.log('Connected to the election database.')
 );
